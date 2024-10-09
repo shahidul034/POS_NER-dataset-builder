d4b213b0626b91123fd826aa531c57b561548c21
Shahidul Shakib <31099713+shahidul034@users.noreply.github.com>
2024-10-09 11:28:59 +0600

    Update dataset_creation.py

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import nltk
 nltk.download('punkt_tab')
+pos_tags_list={'"': 0, "''": 1, '#': 2, '$': 3, '(': 4, ')': 5, ',': 6, '।': 7, ':': 8, '``': 9, 'CC': 10, 'CD': 11, 'DT': 12,
+ 'EX': 13, 'FW': 14, 'IN': 15, 'JJ': 16, 'JJR': 17, 'JJS': 18, 'LS': 19, 'MD': 20, 'NN': 21, 'NNP': 22, 'NNPS': 23,
+ 'NNS': 24, 'NN|SYM': 25, 'PDT': 26, 'POS': 27, 'PRP': 28, 'PRP$': 29, 'RB': 30, 'RBR': 31, 'RBS': 32, 'RP': 33,
+ 'SYM': 34, 'TO': 35, 'UH': 36, 'VB': 37, 'VBD': 38, 'VBG': 39, 'VBN': 40, 'VBP': 41, 'VBZ': 42, 'WDT': 43,
+ 'WP': 44, 'WP$': 45, 'WRB': 46}
+ner_tags_list={'O': 0, 'B-ADJP': 1, 'I-ADJP': 2, 'B-ADVP': 3, 'I-ADVP': 4, 'B-CONJP': 5, 'I-CONJP': 6, 'B-INTJ': 7, 'I-INTJ': 8,
+ 'B-LST': 9, 'I-LST': 10, 'B-NP': 11, 'I-NP': 12, 'B-PP': 13, 'I-PP': 14, 'B-PRT': 15, 'I-PRT': 16, 'B-SBAR': 17,
+ 'I-SBAR': 18, 'B-UCP': 19, 'I-UCP': 20, 'B-VP': 21, 'I-VP': 22}
+pos_tags_reverse = {v: k for k, v in pos_tags_list.items()}
+ner_tags_reverse = {v: k for k, v in ner_tags_list.items()}
 def dataset_gen():
     from datasets import load_from_disk
     dataset = load_from_disk("data")
@@ -8,7 +18,7 @@ def dataset_gen():
 
 def already_completed_show(id):
     import pandas as pd
-    import gradio as gr  # Assuming you're using Gradio
+    import gradio as gr  
     import ast
 
     # Read the Excel file
@@ -44,8 +54,8 @@ def already_completed_show(id):
             html_table += f"""
                 <tr>
                     <td>{token}</td>
-                    <td>{pos_tag}</td>
-                    <td>{ner_tag}</td>
+                    <td>{pos_tag}({pos_tags_reverse[int(pos_tag)]})</td>
+                    <td>{ner_tag}({ner_tags_reverse[int(ner_tag)]})</td>
                 </tr>
             """
         
@@ -97,8 +107,8 @@ def show_tok(tran_text,flag=1):
             t1,t2=temp[:len(temp)-1],temp[len(temp)-1]
             tokens[len(tokens)-1]=t1
             tokens.append(t2)
-        else:
-            tokens.append("।")
+        # else:
+        #     tokens.append("।")
     if flag==0:
         return tokens
     return {
@@ -112,13 +122,15 @@ def display_table(sent_no,flag=1):
         temp = pd.read_excel(file_path)
         print("File exists and has been loaded.")
     else:
-        temp = pd.DataFrame(columns=['id', 'tokens', 'pos_tag', 'ner_tag'])
+        temp = pd.DataFrame(columns=['id', 'tokens', 'pos_tags', 'ner_tags'])
         temp.to_excel(file_path, index=False)
 
     if temp.isin([int(sent_no)]).any().any()==True:
         gr.Warning("An answer already exists! If you submit another one, it will replace the previous answer.")
     
     df = pd.DataFrame(dataset[sent_no])
+    df['pos_tags'] = df['pos_tags'].apply(lambda x: f"{x}({pos_tags_reverse[x]})")
+    df['ner_tags'] = df['ner_tags'].apply(lambda x: f"{x}({ner_tags_reverse[x]})")
     df=df.drop(['id','chunk_tags'],axis=1)
     text=" ".join(dataset[int(sent_no)]['tokens'])
     df_with_custom_index = df.copy()
@@ -257,6 +269,91 @@ with gr.Blocks(css=css) as demo:
         save = gr.Button("Save the data")
     with gr.Row():
         lab=gr.Label(visible=False)
+    with gr.Row():
+        gr.Markdown(
+            """
+## POS Tags Documentation
+
+- `"` : 0 - Quotation mark (open or close)
+- `''` : 1 - Closing quotation mark
+- `#` : 2 - Number sign (e.g., hashtags)
+- `$` : 3 - Dollar sign
+- `(` : 4 - Left parenthesis
+- `)` : 5 - Right parenthesis
+- `,` : 6 - Comma
+- `।` : 7 - Bengali punctuation mark for a full stop (period)
+- `:` : 8 - Colon
+- ``` ` ``` : 9 - Opening quotation mark
+- `CC` : 10 - Coordinating conjunction (e.g., *and*, *but*)
+- `CD` : 11 - Cardinal number (e.g., *one*, *two*)
+- `DT` : 12 - Determiner (e.g., *the*, *a*)
+- `EX` : 13 - Existential "there" (e.g., *there is*)
+- `FW` : 14 - Foreign word
+- `IN` : 15 - Preposition or subordinating conjunction (e.g., *in*, *on*, *because*)
+- `JJ` : 16 - Adjective (e.g., *big*, *old*)
+- `JJR` : 17 - Adjective, comparative (e.g., *bigger*, *older*)
+- `JJS` : 18 - Adjective, superlative (e.g., *biggest*, *oldest*)
+- `LS` : 19 - List item marker (e.g., *1.*, *2.*)
+- `MD` : 20 - Modal verb (e.g., *can*, *should*)
+- `NN` : 21 - Noun, singular or mass (e.g., *cat*, *laughter*)
+- `NNP` : 22 - Proper noun, singular (e.g., *John*, *Paris*)
+- `NNPS` : 23 - Proper noun, plural (e.g., *Americans*)
+- `NNS` : 24 - Noun, plural (e.g., *cats*)
+- `NN|SYM` : 25 - Noun or symbol (rare case)
+- `PDT` : 26 - Predeterminer (e.g., *all*, *both*)
+- `POS` : 27 - Possessive ending (e.g., *'s*)
+- `PRP` : 28 - Personal pronoun (e.g., *I*, *he*, *they*)
+- `PRP$` : 29 - Possessive pronoun (e.g., *my*, *his*, *their*)
+- `RB` : 30 - Adverb (e.g., *quickly*, *never*)
+- `RBR` : 31 - Adverb, comparative (e.g., *faster*)
+- `RBS` : 32 - Adverb, superlative (e.g., *fastest*)
+- `RP` : 33 - Particle (e.g., *up*, *off*)
+- `SYM` : 34 - Symbol (e.g., math or currency symbols)
+- `TO` : 35 - "To" as a preposition or infinitive marker (e.g., *to go*)
+- `UH` : 36 - Interjection (e.g., *uh*, *well*)
+- `VB` : 37 - Verb, base form (e.g., *run*, *eat*)
+- `VBD` : 38 - Verb, past tense (e.g., *ran*, *ate*)
+- `VBG` : 39 - Verb, gerund or present participle (e.g., *running*, *eating*)
+- `VBN` : 40 - Verb, past participle (e.g., *run*, *eaten*)
+- `VBP` : 41 - Verb, non-3rd person singular present (e.g., *I run*)
+- `VBZ` : 42 - Verb, 3rd person singular present (e.g., *he runs*)
+- `WDT` : 43 - Wh-determiner (e.g., *which*, *that*)
+- `WP` : 44 - Wh-pronoun (e.g., *who*, *what*)
+- `WP$` : 45 - Possessive wh-pronoun (e.g., *whose*)
+- `WRB` : 46 - Wh-adverb (e.g., *how*, *where*)
+
+---
+
+## NER Tags Documentation
+
+- `O` : 0 - Outside of any named entity
+- `B-ADJP` : 1 - Beginning of an adjective phrase
+- `I-ADJP` : 2 - Inside an adjective phrase
+- `B-ADVP` : 3 - Beginning of an adverb phrase
+- `I-ADVP` : 4 - Inside an adverb phrase
+- `B-CONJP` : 5 - Beginning of a conjunction phrase
+- `I-CONJP` : 6 - Inside a conjunction phrase
+- `B-INTJ` : 7 - Beginning of an interjection
+- `I-INTJ` : 8 - Inside an interjection
+- `B-LST` : 9 - Beginning of a list item
+- `I-LST` : 10 - Inside a list item
+- `B-NP` : 11 - Beginning of a noun phrase (e.g., *the big dog*)
+- `I-NP` : 12 - Inside a noun phrase
+- `B-PP` : 13 - Beginning of a prepositional phrase (e.g., *in the house*)
+- `I-PP` : 14 - Inside a prepositional phrase
+- `B-PRT` : 15 - Beginning of a particle phrase (e.g., *up* in *give up*)
+- `I-PRT` : 16 - Inside a particle phrase
+- `B-SBAR` : 17 - Beginning of a subordinate clause (e.g., *that he left*)
+- `I-SBAR` : 18 - Inside a subordinate clause
+- `B-UCP` : 19 - Beginning of an unlike coordinated phrase
+- `I-UCP` : 20 - Inside an unlike coordinated phrase
+- `B-VP` : 21 - Beginning of a verb phrase (e.g., *ran quickly*)
+- `I-VP` : 22 - Inside a verb phrase
+
+
+"""
+
+        )
     btn_ques.click(display_table,ques,[show_text1,show_text2,num_text,translation_options])
     def translation_change(translation_options,sent_no):
         english_text, options=display_text(sent_no)
